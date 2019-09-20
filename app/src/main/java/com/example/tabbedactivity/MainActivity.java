1ee1e396c9fe69d2ec2829af2dac610849eb3028
xwaxes01 <xwaxes@gmail.com>
2019-09-20 13:02:48 +0300

    UI change and added Action Bar Menu

--- app/src/main/java/com/example/tabbedactivity/MainActivity.java
+++ app/src/main/java/com/example/tabbedactivity/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.tabbedactivity;
 import android.annotation.TargetApi;
 import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,15 +21,17 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainFragment";
     private SectionsPageAdapter mSectionsPageAdapter;
     private ViewPager mViewPager;
+    private Toolbar mTopToolbar;
 
-    @TargetApi(24)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTopToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+
         Log.d(TAG, "onCreate: Starting...");
-
-
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
@@ -35,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        Creates the menu inflater
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        Handles clicks on clicked menu items
+        int id = item.getItemId();
+
+        if (id==R.id.darktheme){
+            Toast.makeText(this, "Feature coming soon", Toast.LENGTH_SHORT).show();
+        }
+        if (id==R.id.about){
+            Toast.makeText(this, "Feature Coming Soon", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 
     @TargetApi(24)
